ae65966b98f7ecb64fcbacaa0baae34e645981f9
goldfinch14344 <115599685+goldfinch14344@users.noreply.github.com>
2022-10-27 14:14:54 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="pdcontextevents",
+    version="0.0.8.1",
+    author="Alma Krizia Fernandez",
+    description="for pipedream constants",
+    packages=["pdcontextevents"]
+)
